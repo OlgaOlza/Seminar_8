45325a75f11feb43ea4bda17f953d49a0f614436
Olga.Olza <olga-rgb0006@rambler.ru>
2023-07-30 12:22:57 +0300

    Поменять местами первую и последнюю строку массива Задача 53

---
+++ Task001/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+void ReverseSomeArray(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[0, i];
+        array[0, i] = array[array.GetLength(0) - 1, i];
+        array[array.GetLength(0) - 1, i] = temp;
+    }
+
+   
+}
+
+void OutputArray(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("| ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("|");
+        Console.WriteLine("");
+    }
+}
+
+void CreateRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+Console.WriteLine("введите количество строк");
+int numberLine = int.Parse(Console.ReadLine());
+Console.WriteLine("введите количество столбцов");
+int numberColumn = int.Parse(Console.ReadLine());
+
+int[,] numbers = new int[numberLine, numberColumn];
+CreateRandomArray(numbers);
+OutputArray(numbers);
+Console.WriteLine();
+ReverseSomeArray(numbers);
+OutputArray(numbers);
